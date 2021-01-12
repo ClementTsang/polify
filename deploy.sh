d04bdf221c9d46c98387f93920e6b607670e4a75
ClementTsang <cjhtsang@uwaterloo.ca>
2021-01-12 01:16:31 -0500

    Fix deploy script?

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 cd polify_demo
 npm run build
+cd ..
 cp polify_demo/dist/* public/
 cd public
 git add *
 git commit -a -m "Pre-push deploy to GitHub Pages"
 git push --no-verify
 cd ..
-git pull
